1100f25d861f086f48c633135028c3958ebadcf7
PawelKrezel <33000010+PawelKrezel@users.noreply.github.com>
2018-12-30 21:33:21 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+var countDownDate = new Date("Jan 1, 2019 00:00:00").getTime();
+
+
+var x = setInterval(function() {
+
+
+  var now = new Date().getTime();
+
+
+  var distance = countDownDate - now;
+
+
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("dni").innerHTML = "&nbsp;<br>"+days+"<br>&nbsp;";
+  document.getElementById("godz").innerHTML = "&nbsp;<br>"+hours+"<br>&nbsp;";
+  document.getElementById("min").innerHTML = "&nbsp;<br>"+minutes+"<br>&nbsp;";
+  document.getElementById("sek").innerHTML = "&nbsp;<br>"+seconds+"<br>&nbsp;";
+
+
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+  }
+}, 1000);
